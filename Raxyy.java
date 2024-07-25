dd0565ad597ff430efe3f1ce4add749d91f67ab9
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2024-07-25 18:00:51 +0530

    Update Raxyy.java
    SWITCH CASE

--- Raxyy.java
+++ Raxyy.java
@@ -4,7 +4,7 @@ public class Raxyy {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-int n = in.nextInt();
+/*int n = in.nextInt();
 int a = 0;
 int b = 1;
 int count = 2;
@@ -16,6 +16,26 @@ while ( count <= n) {
 }
 System.out.println(b);
 }
+}*/
+
+
+/// SWITCH CASE IN JAVA
+
+         String fruit = in.next();
+        switch (fruit) {
+            case "Mango" :
+            System.out.println("King");
+            break;
+            case "Apple":
+            System.out.println("Keeps doctor away");
+            break;
+            case "Orange":
+            System.out.println("Nagpur King");
+            break;
+            default:
+            System.out.println("Please enter valid fruit");
+        }
+    }
 }
 
 
