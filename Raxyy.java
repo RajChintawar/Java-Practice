eed57b422aab60f2008378feacbe66c2d03a7290
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2024-07-25 18:20:49 +0530

    Update Raxyy.java
    switch case example

--- Raxyy.java
+++ Raxyy.java
@@ -38,5 +38,13 @@ System.out.println(b);
     }
 }
 
+/// Switch case neat exxample with int
+/* int day = in.nextInt();
 
-
+switch(day){
+    case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+    case 6, 7 -> System.out.println("Weekend");
+}
+    }
+}
+*/
