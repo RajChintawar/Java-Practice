312d8c19323c46b5909634ef8f341583b1c76187
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2024-09-09 00:12:53 +0530

    Update advanceproblems.java

--- advanceproblems.java
+++ advanceproblems.java
@@ -32,3 +32,20 @@ public class advanceproblems {
         System.out.println("The length of the longest substring without repeating characters is: " + lengthOfLongestSubstring(s));
     }
 }
+
+//// MAX VALUE IN A ARRAY 
+int[] arr = {1, 2, 3, 7, 10, 13};
+System.out.println(max(arr));
+  }
+  static int max(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+   if( arr[i]> max){
+    max = arr[i];
+
+   }
+   
+    }
+    return max;
+  }
+}
