d74c61cad9c1f34bc5dbfa57099f01e71ca8bb27
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-04 12:12:17 +0530

    Update problems.java

--- problems.java
+++ problems.java
@@ -15,3 +15,25 @@ return avg;
 
     }
 }
+
+
+// Write a function to print the sum of all odd numbers from 1 to n.
+
+public static int oddSum(int n){
+    int sum = 0;
+    for (int i=1; i<=n; i++){
+        if( i%2 != 0){
+          sum = sum+i;
+        }
+    }
+    return sum;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+   int reasult = oddSum(n);
+System.out.println(reasult);
+
+}
+}
+
