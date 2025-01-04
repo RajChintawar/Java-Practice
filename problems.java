f0aa3488d0a9a38114336e97759bd87302d2c8a3
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-04 12:29:05 +0530

    Update problems.java
    Write a function which takes in 2 numbers and returns the greater of those two.

--- problems.java
+++ problems.java
@@ -37,3 +37,25 @@ System.out.println(reasult);
 }
 }
 
+
+// Write a function which takes in 2 numbers and returns the greater of those two.
+
+
+public static int greater(int a, int b){
+    if(a>b){
+        return a;
+    }else {
+        return b;
+    }
+
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int reasult = greater(a, b);
+    System.out.println("Greater of two: "+reasult);
+
+}
+}
+
