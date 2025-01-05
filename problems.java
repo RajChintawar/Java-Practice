3689edeeaf2da5aa455de23f97d1976a7a63a024
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-05 23:30:52 +0530

    Update problems.java
    //Write a function that takes in the radius as input and returns the circumference of a circle.

--- problems.java
+++ problems.java
@@ -59,3 +59,21 @@ public static void main(String[] args) {
 }
 }
 
+
+
+
+//Write a function that takes in the radius as input and returns the circumference of a circle.
+
+public static double circumference(double a){
+    return 2*3.14*a;
+
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    double ans = circumference(a);
+System.out.println(" Circumference of circle : " +ans);
+
+}
+}
+
