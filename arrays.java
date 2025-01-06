c899c0484a91ae037fdb55920e88ad502ec6ad3a
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-06 23:21:50 +0530

    Update arrays.java
    //Take an array of names as input from the user and print them on the screen.

--- arrays.java
+++ arrays.java
@@ -28,3 +28,22 @@ System.out.println("Element found at index :"+i );
     
     }
 
+//Take an array of names as input from the user and print them on the screen.
+
+        Scanner sc = new Scanner(System.in);
+        int size= sc.nextInt(); 
+        
+        String[] names = new String[size];
+        
+        
+        //input
+        for (int i = 0; i <size; i++) {
+        names[i] = sc.next();
+            
+        }
+        
+        for (int i = 0; i <size; i++) {
+System.out.print(Arrays.toString(names));
+        }
+    }
+}
