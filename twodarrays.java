75f7e8747a1d08b9ef01189f6491c936a1bed52f
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-09 00:41:16 +0530

    Update twodarrays.java

--- twodarrays.java
+++ twodarrays.java
@@ -3,27 +3,27 @@ public class twodarrays {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //Printing 2 D Array
-//    int rows = in.nextInt();
-//    int columns = in.nextInt();
-//    int[][] number = new int[rows][columns];
+   int rows = in.nextInt();
+   int columns = in.nextInt();
+   int[][] number = new int[rows][columns];
 
-//    for (int i=0; i<rows; i++){
-//     for(int j=0; j<columns; j++){
-//         number[i][j] = in.nextInt();
-//     }
-//    }
+   for (int i=0; i<rows; i++){
+    for(int j=0; j<columns; j++){
+        number[i][j] = in.nextInt();
+    }
+   }
 
-//    for (int i =0; i<rows; i++){
-//     for(int j=0; j<columns; j++){
-//         System.out.print(number[i][j] + " ");
-//     }
-//     System.out.println();
+   for (int i =0; i<rows; i++){
+    for(int j=0; j<columns; j++){
+        System.out.print(number[i][j] + " ");
+    }
+    System.out.println();
 
-//     }
+    }
 
-//    }
+   }
    
-//     }
+    }
 
 
 
