ebaf1186b8b54a7e9c2c0484650c25d917edc5cb
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-09 00:40:54 +0530

    Add files via upload
    Printing and searching in 2D arrrays !!!

---
+++ twodarrays.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class twodarrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Printing 2 D Array
+//    int rows = in.nextInt();
+//    int columns = in.nextInt();
+//    int[][] number = new int[rows][columns];
+
+//    for (int i=0; i<rows; i++){
+//     for(int j=0; j<columns; j++){
+//         number[i][j] = in.nextInt();
+//     }
+//    }
+
+//    for (int i =0; i<rows; i++){
+//     for(int j=0; j<columns; j++){
+//         System.out.print(number[i][j] + " ");
+//     }
+//     System.out.println();
+
+//     }
+
+//    }
+   
+//     }
+
+
+
+
+//Searching in 2D Array
+
+int rows= in.nextInt();
+int cols = in.nextInt();
+int[][] number = new int[rows][cols];
+
+for (int i = 0; i <rows; i++){
+    for (int j =0; j<cols; j++){
+        number[i][j] = in.nextInt();
+    }
+}
+
+int x = in.nextInt();
+for (int i = 0; i <rows; i++){
+    for (int j =0; j<cols; j++){
+if(number[i][j]==x){
+    System.out.println("x got location ("+i+","+j+")");
+}
+    }
+}
+
+
+
+
+for (int i = 0; i <rows; i++){
+    for (int j =0; j<cols; j++){
+        System.out.print(number[i][j]+" ");
+    }
+    System.out.println();
+
+}
+}
+}
