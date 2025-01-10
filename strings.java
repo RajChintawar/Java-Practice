7e1ef53ae4b26a060600675f148c32e47d6922bc
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-10 16:24:00 +0530

    Update strings.java

--- strings.java
+++ strings.java
@@ -23,10 +23,32 @@ for (int i = 0; i < fullname.length(); i++) {
     System.out.println(fullname.charAt(i));     ///Print each character pre existing. 
 }
 
+//Comparision in strings !!!
+String name1 = "Om";
+String name2 = "Raj";
+if(name1.compareTo(name2)==0) {     //.compareTo() function to compare Strings
+    System.out.println("Both Strings are equal");
+ }else{
+    System.out.println("Strings are unequal");
+}
+
+
+//.substring function in which starting index is given and ending while the characters between are given as output.
+String name = "Drunk Forever";
+String nane = name.substring(6, name.length());
+ System.out.println(nane); 
+}
+
+
+
+
+
+
+
 
 
 
 }
-}
+
 
  
