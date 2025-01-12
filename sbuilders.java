12aa1780bbb2eb137e70fc75b6ca4cb892a4f17d
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-12 16:01:09 +0530

    Update sbuilders.java

--- sbuilders.java
+++ sbuilders.java
@@ -1,27 +1,27 @@
 public class sbuilders {
     public static void main(String[] args) {
         StringBuilder rac = new StringBuilder("Soham");
-//         System.out.println(rac);
+        System.out.println(rac);
 
-// //SET CHAR AT INDEX 0 
-//         rac.setCharAt(0, 'P');
-//         System.out.println(rac);
+//SET CHAR AT INDEX 0 
+        rac.setCharAt(0, 'P');
+        System.out.println(rac);
 
-// ///INSERT CHAR AT INDEX
-// rac.insert(0, 'S');
-// System.out.println(rac);
-
-
-// /// DELETE CHAR AT INDEX
-// rac.deleteCharAt(1);
-// System.out.println(rac);
+///INSERT CHAR AT INDEX
+rac.insert(0, 'S');
+System.out.println(rac);
 
 
+/// DELETE CHAR AT INDEX
+rac.deleteCharAt(1);
+System.out.println(rac);
 
 
-// //ADD CHAR AT LAST
-// rac.append("e");
-// System.out.println(rac);
+
+
+//ADD CHAR AT LAST
+rac.append("e");
+System.out.println(rac);
 
 
 
