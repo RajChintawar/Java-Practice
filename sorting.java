265494f1f23812b74f5d477cf4cbcff285592528
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-15 23:07:14 +0530

    Add files via upload
    Bubblesort

---
+++ sorting.java
@@ -0,0 +1,25 @@
+
+
+public class sorting {
+
+    public static void printArray(int arr[]){
+        for (int i = 0; i <arr.length; i++) {
+           System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    //bubble Sort
+    public static void main(String[] args) {
+        int arr[] = {7,3,2,8,9};
+        for (int i = 0; i <arr.length-1; i++) {
+          for (int j = 0; j < arr.length-i-1; j++) {
+              if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]=temp;
+              }
+          }  
+        }
+        printArray(arr);
+    }
+}
