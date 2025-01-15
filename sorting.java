7730cb8664cb822ad27e5bc2a2b8f434183ad492
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-16 00:34:54 +0530

    Update sorting.java
    Selection Sort

--- sorting.java
+++ sorting.java
@@ -23,3 +23,34 @@ public class sorting {
         printArray(arr);
     }
 }
+
+//Selection Sort
+
+public static void selectionArray(int arr[]){
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
+        
+    }
+    System.out.println();
+}
+public static void main(String[] args) {
+    int arr[] ={88,52,50,32,11,29};
+    for (int i = 0; i <arr.length-1; i++) {
+        int smallest = i;
+for (int j = i+1; j < arr.length; j++){
+       if(arr[smallest]>arr[j]){
+        smallest = j;
+       }
+}      
+int temp = arr[smallest];
+arr[smallest]=arr[i];
+arr[i]=temp;
+
+
+
+
+
+    }
+    selectionArray(arr);
+}
+}
