7d71f9d45f42b8fe0a40d6724104aff3317a076f
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-16 00:41:04 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -54,3 +54,30 @@ arr[i]=temp;
     selectionArray(arr);
 }
 }
+
+
+
+
+//INSERTION SORT
+
+public static void selectionArray(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+            
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+       int arr[] ={5,3,9,8,2,1};
+        for (int i = 0; i <arr.length; i++) {
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        selectionArray(arr);
+        }
+    }
