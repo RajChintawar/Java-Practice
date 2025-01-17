fcc7c3c6064d3da3227bacf782f396af47172bf1
Raj Chintawar <161466309+RajChintawar@users.noreply.github.com>
2025-01-17 18:23:03 +0530

    Update recursion1.java
    adding n natural numbers

--- recursion1.java
+++ recursion1.java
@@ -11,3 +11,21 @@ public class recursion1 {
         printnumb(n);
     }
 }
+
+
+
+///Recusrsion add n natural numbers
+public static void printnumb(int i,int n , int sum){
+        if(i==n){
+            sum+=i; 
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        printnumb(i+1, n, sum);
+    }
+    public static void main(String[] args) {
+        printnumb(1, 5, 0);
+        
+    }
+}
