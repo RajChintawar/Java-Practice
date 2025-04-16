067cca2fe4d5e944bbf8411856fa5fcf1ee2e0db
Raxxyy <161466309+RajChintawar@users.noreply.github.com>
2025-04-16 23:45:03 +0530

    Add files via upload

---
+++ encap.java
@@ -0,0 +1,26 @@
+class Human{
+    private int age;
+private String name;
+public int getAge() {
+    return age;
+}
+public void setAge(int age) {
+    this.age = age;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+
+}
+public class encap {
+ public static void main(String[] args) {
+    Human obj = new Human();
+    obj.setAge(50);
+    obj.setName("Raxxy");
+
+    System.out.println(obj.getAge() +":"+ obj.getName() );
+ }   
+}
